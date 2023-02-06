0297c222e04b9bf6de2fbcd3db49700639fa294e
aco12365 <aca12365@outlook.com>
2023-02-06 15:50:46 -0500

    STUFF WAS ADDED

--- Helpers/temp.pl
+++ Helpers/temp.pl
@@ -18,60 +18,63 @@ opposite(left, right) :-    % The opposite of Left is Right
 
 unsafe(state(_,L1,W1,L2,W2)):-
     (W1>0,L1>W1);(W2>0,L2>W2).
+unsafe(state(_,L1,W1,L2,W2)):-
+    L1<0;
+    L2<0;
+    W1<0;
+    W2<0.
 
-move(state(right,L1,W1,L2,W2),state(Left,A,B,C,D)):- % move 1 lion and 1 wildebeest from right to left side of bank 
+move(state(right,L1,W1,L2,W2),state(left,A,B,C,D)):- % move 1 lion and 1 wildebeast from right to left side of bank  CHECK
     (L1 > 0, W1 > 0),
-    opposite(right,Left),
     A is L1 - 1,
     B is W1 - 1,
     C is L2 + 1,
     D is W2 + 1.
 
-move(state(right,L1,W1,L2,W2),state(Side2,A,W1,C,W2)):- % move 1 lion from right to left side of bank 
+move(state(left,L1,W1,L2,W2), state(right,A,B,C,D)):- % move 1 lion and 1 wildabeast from left to right side of bank CHECK
+    (L2 > 0, W2 > 0),
+    A is L1 + 1,
+    B is W1 + 1,
+    C is L2 - 1,
+    D is W2 - 1.
+
+move(state(right,L1,W1,L2,W2),state(left,A,W1,C,W2)):- % move 1 lion from right to left side of bank CHECK
     (L1 > 0),
-    opposite(right,Side2),
     A is L1 - 1,
     C is L2 + 1.
 
-move(state(right,L1,W1,L2,W2),state(Side2,A,W1,C,W2)):- % move 2 lions from right to left side of bank 
+move(state(right,L1,W1,L2,W2),state(left,A,W1,C,W2)):- % move 2 lions from right to left side of bank CHECK
     (L1 > 1),
-    opposite(right,Side2),
     A is L1 - 2,
     C is L2 + 2.
 
-move(state(right,L1,W1,L2,W2),state(Side2,L1,B,L2,D)):- % move 1 wildebeast from right to left side of bank 
+move(state(right,L1,W1,L2,W2),state(left,L1,B,L2,D)):- % move 1 wildebeast from right to left side of bank CHECK
     (W1 > 0),
-    opposite(right,Side2),
     B is W1 - 1,
     D is W2 + 1.
 
-move(state(right,L1,W1,L2,W2),state(Side2,L1,B,L2,D)):- % move 2 wildebeest from right to left side of bank 
+move(state(right,L1,W1,L2,W2),state(left,L1,B,L2,D)):- % move 2 wildebeest from right to left side of bank CHECK
     (W1 > 1),
-    opposite(right,Side2),
     B is W1 - 2,
     D is W2 + 2.
 
-move(state(left,L1,W1,L2,W2),state(Side2,A,W1,C,W2)):- % move 1 lion from left to right side of bank 
+move(state(left,L1,W1,L2,W2),state(right,A,W1,C,W2)):- % move 1 lion from left to right side of bank CHECK
     (L2 > 0),
-    opposite(left,Side2),
     A is L1 + 1,
     C is L2 - 1.
 
-move(state(left,L1,W1,L2,W2),state(Side2,A,W1,C,W2)):- % move 2 lion from left to right side of bank 
+move(state(left,L1,W1,L2,W2),state(right,A,W1,C,W2)):- % move 2 lion from left to right side of bank CHECK
     (L2 > 1),
-    opposite(left,Side2),
     A is L1 + 2,
     C is L2 - 2.
 
-move(state(left,L1,W1,L2,W2),state(Side2,L1,B,L2,D)):- % move 1 wildabeast from left to right side of bank 
+move(state(left,L1,W1,L2,W2),state(right,L1,B,L2,D)):- % move 1 wildabeast from left to right side of bank CHECK
     (W1 > 0),
-    opposite(left,Side2),
     B is W1 + 1,
     D is W2 - 1.
 
-move(state(left,L1,W1,L2,W2),state(Side2,L1,B,L2,D)):- % move 2 wildabeast from left to right side of bank 
+move(state(left,L1,W1,L2,W2),state(right,L1,B,L2,D)):- % move 2 wildabeast from left to right side of bank CHECK
     (W1 > 1),
-    opposite(left,Side2),
     B is W1 + 2,
     D is W2 - 2.
 
@@ -79,6 +82,7 @@ path(S,G,L,L1) :-
 	move(S,S1),
 	not( unsafe(S1) ),
 	not( member(S1,L) ),
+    write(S1),nl,
 	path(S1, G, [S1|L], L1).	
 	%!.						% comment this line to get more than one solution
 path(G,G,T,T) :-
